1cd3b6a82a2affbd41fefa1ff139ed7ff0c4a0dc
Djuanzz <adnanjuan06@gmail.com>
2025-05-18 09:26:15 +0700

    feat: tugas3 progjarD

---
+++ client/file_interface.py
@@ -0,0 +1,49 @@
+import os
+import json
+import base64
+from glob import glob
+
+
+class FileInterface:
+    def __init__(self):
+        os.chdir('files/')
+
+    def list(self,params=[]):
+        try:
+            filelist = glob('*.*')
+            return dict(status='OK',data=filelist)
+        except Exception as e:
+            return dict(status='ERROR',data=str(e))
+
+    def get(self,params=[]):
+        try:
+            filename = params[0]
+            if (filename == ''):
+                return None
+            fp = open(f"{filename}",'rb')
+            isifile = base64.b64encode(fp.read()).decode()
+            return dict(status='OK',data_namafile=filename,data_file=isifile)
+        except Exception as e:
+            return dict(status='ERROR',data=str(e))
+
+    def upload(self, params=[]):
+        try:
+            filename = params[0]
+            print(params)
+            filecontent_b64 = params[1]
+            if filename == '' or filecontent_b64 == '':
+                return dict(status='ERROR', data='parameter tidak lengkap')
+            filecontent = base64.b64decode(filecontent_b64)
+            with open(filename, 'wb') as f:
+                f.write(filecontent)
+            return dict(status='OK', data='file berhasil diupload')
+        except Exception as e:
+            return dict(status='ERROR', data=str(e))
+
+
+
+
+if __name__=='__main__':
+    f = FileInterface()
+    print(f.list())
+    print(f.get(['pokijan.jpg']))
